db96be4c15f61f7df9cd2916fa5b6f32aa558cef
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-02-08 01:59:14 +0300

    chore: use different impl

--- src/lib.rs
+++ src/lib.rs
@@ -190,63 +190,41 @@ impl Soda {
     }
 }
 
-fn setup_logging(verbosity: u64) -> Result<(), fern::InitError> {
-    let mut base_config = fern::Dispatch::new();
+// impl Soda {
+//     // fn _addConfig(&mut self, config: &fern::Dispatch) {
 
-    base_config = match verbosity {
-        0 => {
-            // Let's say we depend on something which whose "info" level messages are too
-            // verbose to include in end-user output. If we don't need them,
-            // let's not include them.
-            base_config
-                .level(log::LevelFilter::Info)
-                .level_for("overly-verbose-target", log::LevelFilter::Warn)
-        }
-        1 => base_config
-            .level(log::LevelFilter::Debug)
-            .level_for("overly-verbose-target", log::LevelFilter::Info),
-        2 => base_config.level(log::LevelFilter::Debug),
-        _3_or_more => base_config.level(log::LevelFilter::Trace),
-    };
+//     // }
+// }
 
-    // Separate file config so we can include year, month and day in file logs
-    let file_config = fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{}[{}][{}] {}",
-                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                record.target(),
-                record.level(),
-                message
-            ))
-        })
-        .chain(fern::log_file("program.log")?);
+// fn setup_logging(verbosity: u64) -> Result<(), fern::InitError> {
+//     let mut base_config = fern::Dispatch::new();
 
-    let stdout_config = fern::Dispatch::new()
-        .format(|out, message, record| {
-            // special format for debug messages coming from our own crate.
-            if record.level() > log::LevelFilter::Info && record.target() == "" {
-                out.finish(format_args!(
-                    "---\nDEBUG: {}: {}\n---",
-                    chrono::Local::now().format("%H:%M:%S"),
-                    message
-                ))
-            } else {
-                out.finish(format_args!(
-                    "[{}][{}][{}] {}",
-                    chrono::Local::now().format("%H:%M"),
-                    record.target(),
-                    record.level(),
-                    message
-                ))
-            }
-        })
-        .chain(std::io::stdout());
+//     base_config = match verbosity {
+//         0 => base_config
+//             .level(log::LevelFilter::Info)
+//             .level_for("overly-verbose-target", log::LevelFilter::Warn),
+//         1 => base_config
+//             .level(log::LevelFilter::Debug)
+//             .level_for("overly-verbose-target", log::LevelFilter::Info),
+//         2 => base_config.level(log::LevelFilter::Debug),
+//         _3_or_more => base_config.level(log::LevelFilter::Trace),
+//     };
 
-    base_config
-        .chain(file_config)
-        .chain(stdout_config)
-        .apply()?;
+//     // Separate file config so we can include year, month and day in file logs
 
-    Ok(())
-}
+//     let file_config = fern::Dispatch::new()
+//         .format(|out, message, record| {
+//             out.finish(format_args!(
+//                 "{}[{}][{}] {}",
+//                 chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+//                 record.target(),
+//                 record.level(),
+//                 message
+//             ))
+//         })
+//         .chain(fern::log_file("program.log")?);
+
+//     base_config.chain(file_config);
+
+//     Ok(())
+// }
